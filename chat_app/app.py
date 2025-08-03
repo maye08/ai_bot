e6016f85e939e738e5719515e56a1f0b359f6473
maye <katyma2013@gmail.com>
2025-08-03 15:41:42 +0800

    fix

--- chat_app/app.py
+++ chat_app/app.py
@@ -1051,12 +1051,8 @@ def create_order():
     try:
         checkout_session = stripe.checkout.Session.create(
             customer_email=user.email,  # 使用用户的email
-            payment_method_types=['card','wechat_pay','alipay','amazon_pay','paypal'],
-            payment_method_options={
-                'wechat_pay': {
-                    'client': 'web'
-                }
-            },
+            payment_method_types=['card','alipay','amazon_pay','paypal'],
+            
             line_items=[
                 {
                     'price': SUBSCRIPTION_PLANS[plan_type]['price_id'], #### here
